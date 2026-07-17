758b07b8144a678feec91686cfa95983d3fa2d41
Hiteshdhole <hiteshdholee@gmail.com>
2026-07-17 20:29:34 +0530

    create main.cpp and README

---
+++ README.md
@@ -0,0 +1,13 @@
+# THE TUI MUSIC PLAYER FOR LINUX
+
+> > features
+> > -> light weight
+> > -> simple
+> > -> mermory efficent
+
+# IMAGES
+
+# HOW TO INSTALL
+
+% mkdir muzx
+git clone
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+  std::cout << "---------------MUZX----------------" << std::endl;
+  std::cout << "version    : 0.1" << std::endl;
+  std::cout << "created by : hitesh" << std::endl;
+  return 0;
+}
