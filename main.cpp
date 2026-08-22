390eff9876b710d97cd5cfb910f9231624b255e1
Hiteshdhole <hiteshdholee@gmail.com>
2026-08-22 22:44:43 +0530

    Implement basic SFML music player

--- main.cpp
+++ main.cpp
@@ -1,40 +1,98 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
+
 int main() {
-
   sf::Music song;
-  // ask user to input the music
-  std::string filename;
 
-  std::cout << "enter the file name : " << std::endl;
-  std::getline(std::cin, filename);
+  // Playlist
+  std::vector<std::string> playlist;
 
-  // load the music
-  if (!song.openFromFile(home / Hitesh / projects / github / muzx / test.mp3)) {
-    std::cerr << "Error :: Music can not be loaded " << std::endl;
+  playlist.push_back("test1.mp3");
+  playlist.push_back("music/test2.mp3");
+  playlist.push_back("music/test3.mp3");
+
+  // Current song index
+  int currentSong = 0;
+
+  // Load first song
+  if (!song.openFromFile(playlist[currentSong])) {
+    std::cerr << "Error: Music cannot be loaded" << std::endl;
     return 1;
   }
-  // std::cout << "version    : 0.1" << std::endl;
-  // std::cout << "created by : hitesh" << std::endl;
-  // std::cout << "[p] play [o] pasue [q] quit " << std::endl;
+
+  std::cout << "The music loaded successfully" << std::endl;
+  std::cout << "Version    : 0.1 " << std::endl;
+  std::cout << "Created by : Hitesh " << std::endl;
+  std::cout << "[p] Play [o] Pause [q] Quit [n] Next [b] Previous" << std::endl;
+  ;
 
   std::string command;
   bool is_running = true;
+
   while (is_running) {
-    std::cout << "->" << std::endl;
+    std::cout << "-> ";
     std::cin >> command;
 
     if (command == "q") {
-      std::cout << "good bye" << std::endl;
+      std::cout << "Goodbye" << std::endl;
       is_running = false;
-    } else if (command == "p") {
-      std::cout << "the song is playing " << std::endl;
-    } else if (command == "o") {
-      std::cout << "the song is pasue " << std::endl;
-    } else {
-      std::cout << "command error " << std::endl;
+    }
+
+    else if (command == "p") {
+      song.play();
+      std::cout << "The song is playing" << std::endl;
+    }
+
+    else if (command == "o") {
+      song.pause();
+      std::cout << "The song is paused" << std::endl;
+    }
+
+    else if (command == "n") {
+      if (currentSong < playlist.size() - 1) {
+        currentSong++;
+
+        song.stop();
+
+        if (!song.openFromFile(playlist[currentSong])) {
+          std::cerr << "Error: Could not load next song" << std::endl;
+        } else {
+          song.play();
+
+          std::cout << "Playing: " << playlist[currentSong] << '\n'
+                    << std::endl;
+        }
+      } else {
+        std::cout << "Already at the last song" << std::endl;
+      }
+    }
+
+    else if (command == "b") {
+      if (currentSong > 0) {
+        currentSong--;
+
+        song.stop();
+
+        if (!song.openFromFile(playlist[currentSong])) {
+
+          std::cerr << "Error: Could not load previous song " << std::endl;
+        } else {
+          song.play();
+
+          std::cout << "Playing: " << playlist[currentSong] << '\n'
+                    << std::endl;
+        }
+      } else {
+        std::cout << "Already at the first song" << std::endl;
+      }
+    }
+
+    else {
+      std::cout << "Command error" << std::endl;
     }
   }
+
   return 0;
 }
